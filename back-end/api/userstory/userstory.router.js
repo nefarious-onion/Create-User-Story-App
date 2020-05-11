6729a98fc4edc972a147d7aad60b5c1b6a9f73f0
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-11 18:18:44 +0300

    add api endpoints for userstories

--- back-end/api/userstory/userstory.router.js
+++ back-end/api/userstory/userstory.router.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const express = require('express');
 const router = express.Router();
 const userstoryService = require('./userstory.service');
@@ -6,7 +8,132 @@ const userstoryService = require('./userstory.service');
 router.get('/', async (req, res) => {
     console.log('get all userstories');
 
-    const stories = await userstoryService.getStories();
+    try {
+        const stories = await userstoryService.getStories();
+        res.json(stories);
+    } catch (error) {
+        console.log(error);
+        res
+            .status(500)
+            .json({
+                message: "Server error"
+            })
+    }
 
-    res.json(stories);
 })
+
+//get one userstory
+router.get('/:id', async (req, res) => {
+    const id = String(req.params.id);
+    console.log(`get one userstory with id ${id}`);
+    try {
+        const story = await userstoryService.getStory(id);
+        console.log({ story });
+
+        if (story) {
+            res.json(story);
+        } else {
+            res
+                .status(404)
+                .json({
+                    message: "Story not found"
+                });
+        }
+    } catch (error) {
+        console.log(error)
+        res
+            .status(500)
+            .json({
+                message: "Server error"
+            })
+    }
+
+});
+
+//create userstory
+router.post('/', async (req, res) => {
+    console.log('create new userstory');
+    const { body } = req;
+    
+    try {
+        const newStory = await userstoryService.createStory(body);
+
+    if (newStory !== false) {
+        console.log('new userstory created ', newStory);
+        res
+            .status(201)
+            .json(newStory);
+    } else {
+        res
+            .status(404)
+            .json({
+                message: "Story not created"
+            })
+    }
+    } catch (error) {
+        console.log(error);
+        res
+            .status(500)
+            .json({
+                message: "Server error"
+            })
+    }
+    
+})
+//update a userstory
+router.patch('/:id', async (req, res) => {
+    const id = String(req.params.id);
+    console.log(`update userstory with the id of ${id}`);
+    const { body } = req;
+
+    try {
+        const updatedStory = await userstoryService.patchStory(id, body);
+
+        if (updatedStory !== false) {
+            console.log('userstory patched', updatedStory);
+            res.json(updatedStory);
+        } else {
+            res
+                .status(404)
+                .json({
+                    message: "Story not found"
+                })
+        }
+    } catch (error) {
+        console.log(error)
+        res
+            .status(500)
+            .json({
+                message: "Server error"
+            })
+    }
+})
+
+//delete userstory
+router.delete('/:id', async (req, res) => {
+    const id = String(req.params.id);
+    console.log(`delete userstory with id of ${id}`);
+
+    try {
+        const deletedStory = await userstoryService.deleteStory(id);
+
+        if (deletedStory !== false) {
+            console.log('userstory deleted', deletedStory);
+            res.json(deletedStory);
+        } else {
+            res
+                .status(204)
+                .json();
+        }
+    } catch (error) {
+        console.log(error);
+        res
+            .status(500)
+            .json({
+                message: "Server error"
+            })
+    }
+
+  
+})
+module.exports = router;
