842d8d3b9207e5e56920c23e445e917ec40152a3
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-11 18:18:22 +0300

    add service functions

--- back-end/api/userstory/userstory.service.js
+++ back-end/api/userstory/userstory.service.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const Userstory = require('./userstory.model');
 
 const getStories = () => {
@@ -12,6 +14,59 @@ const getStories = () => {
     })
 }
 
+const getStory = id => {
+    const isIdValid = typeof (id) === "string";
+
+    if (!isIdValid) {
+        throw new Error('Id is not a string');
+    } else {
+        return new Promise((resolve, reject) => {
+            Userstory.findById(id, (error, story) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(story);
+                }
+            })
+        })
+    }
+}
+
+const createStory = content => {
+    try {
+        const story = new Userstory(content);
+        return story.save();
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+const patchStory = async (id, updatedContent) => {
+    try {
+        const {_id, ...restContent} = updatedContent;
+        const updatedStory = await Userstory.findOneAndUpdate({_id: id }, restContent);
+        return updatedStory;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+const deleteStory = async (id) => {
+    try {
+        const deletedStory = await Userstory.findOneAndDelete({_id: id});
+        return deletedStory;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
 module.exports = {
-    getStories
+    getStories,
+    getStory,
+    createStory,
+    patchStory,
+    deleteStory,
 }
