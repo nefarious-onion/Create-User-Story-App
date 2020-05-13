22018144dae7a3b7a17bc34a7e109f2e5b5b068e
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-13 10:55:00 +0300

    add userstory component

---
+++ front-end/src/components/Userstory/Userstory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Userstory = () => {
+    return (
+        <div>
+            <h3>Userstory</h3>
+            <p>This is a userstory</p>
+        </div>
+    );
+}
+
+export default Userstory;
