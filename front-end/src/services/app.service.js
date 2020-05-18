a9e5541eaadaf3012bb1fd83dfe0541422b86f64
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-18 23:53:00 +0300

    add axios request logic

--- front-end/src/services/app.service.js
+++ front-end/src/services/app.service.js
@@ -1,5 +1,29 @@
 import axios from 'axios';
 
-const EPIC_URL = 'http://localhost:8000/epic';
+const EPIC_URL = 'http://localhost:8000/epic/';
 
 export const getEpics = () => axios.get(EPIC_URL).then(response => response.data);
+
+export const createEpic = async (epicName) => {
+    const savedEpic = { title: epicName }
+    try {
+        const response = await axios.post(EPIC_URL, savedEpic);
+        console.log('epic saved', response.data);
+    } catch (error) {
+        console.log('epic not saved', error)
+    }
+}
+
+export const getEpic = id => axios.get(EPIC_URL + id).then(response => response.data);
+
+export const createStory = async (newstory, epicId) => {
+    const STORY_URL = EPIC_URL + `${epicId}/userstory`;
+    const  savedStory = { title: newstory };
+
+    try {
+        const response = await axios.post(STORY_URL, savedStory);
+        console.log('story saved', response.data);
+    } catch (error) {
+        console.log('story not saved', error);
+    }
+}
