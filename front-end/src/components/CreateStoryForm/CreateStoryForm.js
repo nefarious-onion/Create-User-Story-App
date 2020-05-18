0b57066e5311676f97cdda98dae7cfcbd69af192
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-18 23:54:42 +0300

    add form for creating userstories

--- front-end/src/components/CreateStoryForm/CreateStoryForm.js
+++ front-end/src/components/CreateStoryForm/CreateStoryForm.js
@@ -1,12 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
-const CreateStoryForm = () => {
-    const [title, setTitle] = useState();
+const CreateStoryForm = ({ onStoryCreate }) => {
+    //const [userInput, setUserInput] = useState("");
+    const [storyUser, setStoryUser] = useState("");
+    const [storyWant, setStoryWant] = useState("");
+    const [storyValue, setStoryValue] = useState("");
+
+    const inputElement = useRef(null);
+    
+    const onSubmit = event => {
+        event.preventDefault();
+
+        const userstory = `As a ${storyUser}, I want to ${storyWant}, so that ${storyValue}`;
+        console.log(userstory);
+
+        onStoryCreate(userstory);
+        setStoryUser("");
+        setStoryWant("");
+        setStoryValue("");
+        inputElement.current.focus();
+    }
 
     return (
-        <form>
-            
-        </form>
+        <div className='form-container'>
+            <form className='userstory-form' onSubmit={onSubmit}>
+                <div className='form__input-field'>
+                    <label>As a</label>
+                    <input type='text' name='storyUser' value={storyUser} onChange={event => setStoryUser(event.target.value)} autoFocus ref={inputElement} required/>
+                </div>
+                <div className='form__input-field'>
+                    <label>I want to</label>
+                    <input type='text' name='storyWant' value={storyWant} onChange={event => setStoryWant(event.target.value)} required/>
+                </div>
+                <div className='form__input-field'>
+                    <label>so that</label>
+                    <input type='text' name='storyValue' value={storyValue} onChange={event => setStoryValue(event.target.value)} required/>
+                </div>
+                <input type='submit' value='Add userstory'/>
+            </form>
+
+        </div>
     );
 }
 
