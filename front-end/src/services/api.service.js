7316a6593969ba60df7aee3fcdadce86c9b4b0ae
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-20 00:41:38 +0300

    add deletestory, deleteepic

--- front-end/src/services/api.service.js
+++ front-end/src/services/api.service.js
@@ -16,9 +16,19 @@ export const createEpic = async (epicName) => {
 
 export const getEpic = id => axios.get(EPIC_URL + id).then(response => response.data);
 
+export const deleteEpic = async (id) => {
+    try {
+        const response = await axios.delete(EPIC_URL + id);
+        console.log('epic deleted', response.data);
+    } catch (error) {
+        console.log('epic not deleted', error);
+    }
+    
+}
+
 export const createStory = async (newstory, epicId) => {
     const STORY_URL = EPIC_URL + `${epicId}/userstory`;
-    const  savedStory = { title: newstory };
+    const savedStory = { title: newstory };
 
     try {
         const response = await axios.post(STORY_URL, savedStory);
@@ -26,4 +36,28 @@ export const createStory = async (newstory, epicId) => {
     } catch (error) {
         console.log('story not saved', error);
     }
+}
+
+export const deleteStory = async (epicId, storyId) => {
+    const STORY_URL = EPIC_URL + `${epicId}/userstory/${storyId}`;
+
+    try {
+        const response = await axios.delete(STORY_URL);
+        console.log('story deleted', response.data);
+    } catch (error) {
+        console.log('story not deleted', error);
+    }
+
+}
+
+export const getStory = async (epicId, storyId) => {
+    const STORY_URL = EPIC_URL + `${epicId}/userstory/${storyId}`;
+       
+        try {
+            const response = await axios.get(STORY_URL);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+
 }
