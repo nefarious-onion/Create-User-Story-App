516c65aa4c9aefa9b4c3c791dc0c4d74509fdd77
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-19 16:58:02 +0300

    rename import

--- front-end/src/components/Epicview/EpicView.js
+++ front-end/src/components/Epicview/EpicView.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getEpic, createStory } from '../../services/app.service';
+import { getEpic, createStory } from '../../services/api.service';
 import UserstoryList from '../UserstoryList/UserstoryList';
 import CreateStoryForm from '../CreateStoryForm/CreateStoryForm';
 
