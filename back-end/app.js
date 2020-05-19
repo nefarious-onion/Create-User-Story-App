2ace772e216f2fb4e6a5e04ecdc7d4def35b0f39
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-20 01:16:06 +0300

    update for heroku

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,7 @@ const userstoryRouter = require('./api/userstory/userstory.router');
 const epicRouter = require('./api/epic/epic.router');
 const dbService = require('./api/db/db.service');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const FRONTEND_ORIGIN = "http://localhost:3000";
 
 const app = express();
@@ -11,13 +11,20 @@ dbService.connect();
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 //allow chrome to do ajax call
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     next();
-})
+});
 
 app.use('/epic', epicRouter);
 app.use('/epic/:id/userstory', userstoryRouter);
