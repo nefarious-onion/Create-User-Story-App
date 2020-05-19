a7c6fe1219399f63e15e084158f727ea35322f84
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-20 00:41:55 +0300

    update props

--- front-end/src/components/UserstoryList/UserstoryList.js
+++ front-end/src/components/UserstoryList/UserstoryList.js
@@ -3,9 +3,9 @@ import Userstory from '../Userstory/Userstory';
 import './UserstoryList.css';
 
 
-const UserstoryList = ({ stories }) => {
+const UserstoryList = ({ stories, onStoryClick }) => {
 
-    const storyList = stories.map(story => <Userstory key={story._id} title={story.title} />)
+    const storyList = stories.map(story => <Userstory id={story._id} onStoryClick={onStoryClick} key={story._id} title={story.title} />)
 
     return (
         <div className='list-container'>
