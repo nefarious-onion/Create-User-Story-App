00afc0818c4c3052677185b6ba66ecf4cebc297b
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-20 00:43:22 +0300

    add stories

--- front-end/src/components/Epicview/EpicView.js
+++ front-end/src/components/Epicview/EpicView.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getEpic, createStory } from '../../services/api.service';
+import { getEpic, createStory, getStory, deleteStory } from '../../services/api.service';
 import UserstoryList from '../UserstoryList/UserstoryList';
 import CreateStoryForm from '../CreateStoryForm/CreateStoryForm';
+import EditStoryView from '../EditStoryView/EditStoryView';
+import './EpicView.css';
 
-const EpicView = ({ onEpicLoad }) => {
+const EpicView = ({ onEpicLoad, onEpicDelete }) => {
     const [epic, setEpic] = useState();
     const [stories, setStories] = useState([]);
     const [epicTitle, setEpicTitle] = useState("");
+    const [editVisible, setEditVisible] = useState(false);
+    const [storyId, setStoryId] = useState("");
+    const [story, setStory] = useState("");
 
     let { epicId } = useParams();
 
@@ -24,23 +29,64 @@ const EpicView = ({ onEpicLoad }) => {
         fetchStories();
     }
 
+    const onStoryClick = async (id) => {
+        setEditVisible(true);
+        setStoryId(id);
+    }
+
+    const onStoryDelete = async () => {
+        await deleteStory(epicId, storyId);
+        setStoryId("");
+        setStory("");
+        setEditVisible(false);
+        fetchStories();
+    }
+    const onDeleteClick = (event) => {
+        if (window.confirm('Are you sure you want to delete this epic?'))  {
+            onEpicDelete(epicId);
+            setEpic("")
+            setEpicTitle("");
+            setStories([]);
+        }
+    }
+
     useEffect(() => {
         getEpic(epicId).then(epic => {
             setEpic(epic);
             setStories(epic.stories);
             setEpicTitle(epic.title);
             onEpicLoad(epic);
+            setStoryId("");
         })
     }, [epicId]);
 
+    useEffect(() => {
+        getStory(epicId, storyId).then(userstory => {
+            setStory(userstory);
+        })
+    }, [storyId]);
+
     return (
+
         <div className='epicview-container'>
-            <div className='epicview__title'>
-            <p>Create userstories for {epicTitle} </p>
-            </div>
+            <div className='userstoryview-container'>
+                {/* <div className='epicview__title'>
+                    <p>Create userstories for {epicTitle} </p>
+                    <button className='button delete-button' >Delete epic {epicTitle}</button>
+                </div> */}
+                <button className='button delete-button' onClick={onDeleteClick} >Delete epic {epicTitle}</button>
             <CreateStoryForm onStoryCreate={onStoryCreate} />
-            <UserstoryList stories={stories} />
+            <UserstoryList stories={stories} onStoryClick={onStoryClick} />
         </div>
+        <div className='editview-container'>
+            {
+                story && (
+                    <EditStoryView story={story} onStoryDelete={onStoryDelete} />
+                )
+            }
+            {/* <EditStoryView editVisible={editVisible} storyId={storyId} epicId={epicId} onStoryDelete={onStoryDelete} /> */}
+        </div>
+        </div >
     );
 }
 
