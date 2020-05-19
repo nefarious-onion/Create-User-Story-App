7f64ea525be47b246fedf3d9a4e010e95c4e9b8f
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-19 18:51:33 +0300

    add epiclist component

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+//import {Link} from "react-router-dom";
 import CreateEpicForm from '../CreateEpicForm/CreateEpicForm';
 import './Sidebar.css';
+import EpicList from '../EpicList/EpicList';
 
 const Sidebar = ({epics, onEpicCreate }) => {
 
 
-const epicList = epics.map(epic => <Link to={`/epic/${epic._id}`} className='sidebar__list-item' key={epic._id}>{epic.title}</Link>)
+//const epicList = epics.map(epic => <Link to={`/epic/${epic._id}`} className='sidebar__list-item' key={epic._id}>{epic.title}</Link>)
     return (
         <div className='sidebar-container'>
             <CreateEpicForm onEpicCreate={onEpicCreate}/>
-            {epicList}
+            <EpicList epics={epics}/>
         </div>
     );
 }
