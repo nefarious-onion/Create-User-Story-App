5f8372c138c27c4ba46359c964f3305611e9eb21
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-20 00:43:04 +0300

    add styling, fix typo

--- front-end/src/components/LandingView/LandingView.js
+++ front-end/src/components/LandingView/LandingView.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CreateEpicForm from '../CreateEpicForm/CreateEpicForm';
+import './LandingView.css';
 
 const LandingView = ({ onEpicCreate }) => {
     return (
-        <div>
+        <div className='landingview-container'>
             <h1>Welcome to Create User Stories App!</h1>
-            <p>Start by creating a new epic or choose an existing ones from the sidebar</p>
+            <p>Start by creating a new epic or choose an existing one from the sidebar</p>
             <CreateEpicForm onEpicCreate={onEpicCreate} />
         </div>
     );
