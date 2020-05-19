bef630eb0ec647c1c9462bff985f9ab6c99c6169
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-20 00:55:46 +0300

    add mongodbatlas

--- back-end/api/db/db.service.js
+++ back-end/api/db/db.service.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const MONGODB_SERVER = 'mongodb+srv://dbAdmin:kissakoirakettu@cluster0-oekcc.mongodb.net/test?retryWrites=true&w=majority';
+const LOCAL_DB = 'mongodb://localhost/create_userstories';
 
 const connect = () => {
-    mongoose.connect('mongodb://localhost/create_userstories', { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(MONGODB_SERVER, { useNewUrlParser: true, useUnifiedTopology: true });
 
     const connection = mongoose.connection;
     connection.on('error', error => console.log(error));
