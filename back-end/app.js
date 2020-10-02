e4c9f6ca0587e654b86bf80d5a01b9f2cd3e2255
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-10-02 16:50:29 +0300

    refactor development and production settings

--- back-end/app.js
+++ back-end/app.js
@@ -3,36 +3,43 @@ const userstoryRouter = require('./api/userstory/userstory.router');
 const epicRouter = require('./api/epic/epic.router');
 const dbService = require('./api/db/db.service');
 const path = require('path');
+const FRONTEND_ORIGIN = "http://localhost:3000";
 
 const PORT = process.env.PORT || 8000;
-const FRONTEND_ORIGIN = "http://localhost:3000";
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const app = express();
 dbService.connect();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'../', 'front-end', 'build')));
 
-//allow chrome to do ajax call
-app.use((req, res, next) => {
+
+
+if (isDev) {
+  //allow chrome to do ajax call
+  app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     next();
-});
+  });
+} else {
+  app.use(express.static(path.join(__dirname, '../', 'front-end', 'build')));
 
-app.use('/api/epic', epicRouter);
-app.use('/api/epic/:id/userstory', userstoryRouter);
-
-app.get('/ping', (req, res) => {
-    res.send('hello world');
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'front-end', 'build', 'index.html'));
   });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'front-end', 'build', 'index.html'));
+}
+app.use('/api/epic', epicRouter);
+app.use('/api/epic/:id/userstory', userstoryRouter);
+app.get('/ping', (req, res) => {
+  res.send('hello world');
 });
 
 app.listen(PORT, () => {
-    console.log('Server started', PORT);
+  console.log('Server started', PORT);
+  console.log('NODE_ENV=', process.env.NODE_ENV)
 })
