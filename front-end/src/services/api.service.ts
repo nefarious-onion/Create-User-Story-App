a3beb19e53bb977c332cee9618ff90f9611f6631
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-10-05 00:10:25 +0300

    change into typescript, refactor

--- front-end/src/services/api.service.ts
+++ front-end/src/services/api.service.ts
@@ -1,39 +1,37 @@
 import axios from 'axios';
 import { baseUrl } from '../config';
-
-const EPIC_URL = baseUrl +  'api/epic/';
+import { Epic, Userstory } from '../services/api.interface';
+const EPIC_URL = `${baseUrl}api/epic/`;
 
 export const getEpics = () => axios.get(EPIC_URL).then(response => response.data);
 
-export const createEpic = async (epicName) => {
-    const savedEpic = { title: epicName }
+export const createEpic = async (epic: Epic) => {
     try {
-        const response = await axios.post(EPIC_URL, savedEpic);
-        console.log('epic saved', response.data);
+        const response = await axios.post(EPIC_URL, epic);
+        const createdEpic = response.data;
+        console.log('epic created', createdEpic);
     } catch (error) {
-        console.log('epic not saved', error)
+        console.log('epic not created', error)
     }
 }
 
-export const getEpic = id => axios.get(EPIC_URL + id).then(response => response.data);
+export const getEpic = (id: string )=> axios.get(EPIC_URL + id).then(response => response.data);
 
-export const deleteEpic = async (id) => {
+export const deleteEpic = async (id: string) => {
     try {
-        const response = await axios.delete(EPIC_URL + id);
-        console.log('epic deleted', response.data);
+        await axios.delete(EPIC_URL + id);
+        console.log('epic deleted');
     } catch (error) {
         console.log('epic not deleted', error);
     }
-      
 }
 
-export const createStory = async (newstory, epicId) => {
+export const createStory = async (userstory: Userstory, epicId: string) => {
     const STORY_URL = EPIC_URL + `${epicId}/userstory`;
-    const savedStory = { title: newstory };
-
     try {
-        const response = await axios.post(STORY_URL, savedStory);
-        console.log('story saved', response.data);
+        const response = await axios.post(STORY_URL, userstory);
+        const savedStory = response.data;
+        console.log('story saved', savedStory);
     } catch (error) {
         console.log('story not saved', error);
     }
