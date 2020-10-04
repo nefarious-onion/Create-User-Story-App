a70209e26ce72f3772e43f28ed6e02400b4250bb
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-10-05 00:10:00 +0300

    change into tyoescript

--- front-end/src/components/App.tsx
+++ front-end/src/components/App.tsx
@@ -2,35 +2,38 @@ import React, { useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
 } from "react-router-dom";
+//services, interfaces
 import { getEpics, createEpic, deleteEpic } from '../services/api.service';
+import { Epic  } from '../services/api.interface';
+//components
 import Sidebar from './Sidebar/Sidebar';
 import Topnav from './Topnav/Topnav';
 import LandingView from './LandingView/LandingView';
 import EpicView from './Epicview/EpicView';
+//styles
 import './App.css';
 
 const App = () => {
     const [epics, setEpics] = useState([]);
-    const [epicName, setEpicName] = useState();
+    const [epicName, setEpicName] = useState<string>();
 
     const fetchEpics = async () => {
         const _epics = await getEpics();
         setEpics(_epics)
     }
 
-    const onEpicCreate = async (newEpic) => {
+    const onEpicCreate = async (newEpic: Epic) => {
         console.log('create new epic', newEpic);
 
         await createEpic(newEpic);
         fetchEpics();
     }
 
-    const onEpicLoad = epic => setEpicName(epic.title);
+    const onEpicLoad = (epic: Epic) => setEpicName(epic.title);
 
-    const onEpicDelete = async (id) => {
+    const onEpicDelete = async (id: string) => {
         await deleteEpic(id);
         setEpicName("");
         fetchEpics();
@@ -46,7 +49,7 @@ const App = () => {
             <div className='app-container'>
                 <Topnav title={epicName} />
                 <div className='main-content'>
-                    <Sidebar className='sidebar' epics={epics} onEpicCreate={onEpicCreate} />
+                    <Sidebar epics={epics} onEpicCreate={onEpicCreate} />
                     <Switch>
                         <Route path='/' exact >
                             <LandingView onEpicCreate={onEpicCreate} />
