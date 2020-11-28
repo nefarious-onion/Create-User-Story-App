95e0424c4aa790e747e7a27ece03adf7ebaf33e6
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-11-28 21:54:25 +0200

    fix port

--- back-end/src/utils/config.ts
+++ back-end/src/utils/config.ts
@@ -1,9 +1,7 @@
-//require('dotenv').config()
 import dotenv from 'dotenv';
 dotenv.config()
 
-export const PORT = 8000;
+export const PORT = process.env.PORT || 8000;
 export const MONGODB_URI = process.env.DB_URI as string;
 export const isDev = process.env.NODE_ENV === 'development';
-export const FRONTEND_ORIGIN = "http://localhost:3000";
 export const MODE = process.env.NODE_ENV
