be2f4fe13a33fd8492d2ea502fe5056935c362ee
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-11-28 23:18:11 +0200

    Disable cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,7 @@
-import { PORT, MODE, isDev } from './utils/config';
+import { PORT, MODE } from './utils/config';
 import express from 'express';
 require('express-async-errors');
-import cors from 'cors';
+// import cors from 'cors';
 import path from 'path';
 import { router as userstoryRouter } from './api/userstory/userstory.router';
 import { router as epicRouter } from './api/epic/epic.router';
@@ -11,9 +11,7 @@ import { mongoConnect } from './api/db/db.service';
 const app = express();
 mongoConnect()
 
-if (isDev) {
-  app.use(cors())
-}
+// app.use(cors())
 app.use(express.static(path.join(__dirname, '../', '../', 'front-end', 'build')))
 app.use(express.json())
 
