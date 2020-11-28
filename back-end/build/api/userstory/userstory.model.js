99d83f22d91d19bdc74d817b6c38407e80d1e850
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-11-28 21:56:36 +0200

    add build to gitignore

--- back-end/build/api/userstory/userstory.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = __importDefault(require("mongoose"));
-var userstory_schema_1 = __importDefault(require("./userstory.schema"));
-var Userstory = mongoose_1.default.model('Userstory', userstory_schema_1.default);
-exports.default = Userstory;
