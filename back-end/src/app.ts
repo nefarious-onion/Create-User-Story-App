d0e08958b1f70dbbb1624bed7dedafce0fde134a
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-11-29 22:34:11 +0200

    fix import for express-async-errors, declare a global module for it

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,7 @@
 import { PORT, MODE } from './utils/config';
 import express from 'express';
-require('express-async-errors');
-// import cors from 'cors';
+import 'express-async-errors';
+import cors from 'cors';
 import path from 'path';
 import { router as userstoryRouter } from './api/userstory/userstory.controller';
 import { router as epicRouter } from './api/epic/epic.controller';
@@ -12,7 +12,7 @@ import { mongoConnect } from './api/db/db.service';
 const app = express();
 mongoConnect()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.static(path.join(__dirname, '../', '../', 'front-end', 'build')))
 app.use(express.json())
 
