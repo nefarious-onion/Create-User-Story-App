8d500d8c33253d6d53febad4c2967a3f26e55a8a
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-11-30 02:10:10 +0200

    fix baseUrl

--- front-end/src/config.ts
+++ front-end/src/config.ts
@@ -1,3 +1,3 @@
 const isDev = window.location.hostname === 'localhost';
 
-export const baseUrl = isDev ? 'http://localhost:8000/' : '/'; 
+export const baseUrl = isDev ? 'http://localhost:8000/api' : '/api'; 
