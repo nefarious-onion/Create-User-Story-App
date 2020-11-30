c00cc2735b6ea433118106973ab69eac15f8c3c5
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-11-30 03:00:41 +0200

    remove mergeParams

--- back-end/src/api/userstory/userstory.controller.ts
+++ back-end/src/api/userstory/userstory.controller.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 import userstoryService from './userstory.service';
 
 //get all userstories
